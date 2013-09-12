f63d646b2fbb53c0bf1270a05b9d6fa449368b20
fay <toozoofoo@gmail.com>
2013-09-12 19:47:34 +0800

    add member list

--- project/tests.py
+++ project/tests.py
@@ -47,27 +47,3 @@ class SimpleTest(TestCase):
         project.add_member(User.objects.all()[1])
         self.assertTrue(project.memberships.count() == 1)
 
-    def est_create_slice(self):
-        slice = Slice(name="test_slice", description=" ",
-                owner=User.objects.all()[0],
-                project=Project.objects.all()[0],
-                date_expired=datetime.datetime.now())
-        slice.save()
-
-        #: add controller
-        controller = Controller(ip="192.168.5.41", port="8081",
-                http_port="8080", username="test", password="test",
-                hostname="test_contoller", island=Island.objects.all()[0])
-        controller.save()
-        controller.slices.add(slice)
-
-        #: add flowvisor
-        flowvisor = Flowvisor(ip="192.168.5.41", port="8081",
-                http_port="8080", username="test", password="test",
-                hostname="test_flowvisor", island=Island.objects.all()[0])
-
-        flowvisor.save()
-        flowvisor.slices.add(slice)
-
-        self.assertTrue(slice.controller_set.all().count() == 1)
-        self.assertTrue(slice.id)
