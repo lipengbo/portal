ba830d0671ab7aa05f2b398c9c6b1f6572892c61
cjx <cjx@cjx.cjx>
2013-09-18 15:33:43 +0800

    unittest

--- slice/tests.py
+++ slice/tests.py
@@ -20,131 +20,131 @@ from plugins.openflow.flowspace_api import flowspace_nw_add
 class SimpleTest(TestCase):
     fixtures = ['cjxunittest.json']
 
-#     def test_create_slice_step(self):
-#         print 'test_create_slice_step'
-#         project = Project.objects.all()[0]
-#         island = project.islands.all()[0]
-#         ovs_ports = SwitchPort.objects.all()
-#         try:
-#             slice_obj = create_slice_step(project,
-#                 'cjxunittestslice', 'description',
-#                 island, project.owner,
-#                 ovs_ports, island.controller_set.all()[0])
-#         except Exception, ex:
-#             print ex
-#             flowvisor_del_slice(island.flowvisor_set.all()[0], 'cjxunittestslice')
-#             self.assertFalse(True)
-#         else:
-#             flowvisor_del_slice(island.flowvisor_set.all()[0], 'cjxunittestslice')
-#             self.assertTrue(slice_obj.id)
-# 
-#     def test_create_slice_api(self):
-#         print 'test_create_slice_api'
-#         project = Project.objects.all()[0]
-#         island = project.islands.all()[0]
-#         try:
-#             slice_obj = create_slice_api(project, 'cjxunittestslice',
-#                 'description', island, project.owner)
-#         except Exception, ex:
-#             print ex
-#             self.assertFalse(True)
-#         else:
-#             self.assertTrue(slice_obj.id)
-# 
-#     def test_slice_change_description(self):
-#         print 'test_slice_change_description'
-#         project = Project.objects.all()[0]
-#         island = project.islands.all()[0]
-#         try:
-#             slice_obj = create_slice_api(project, 'cjxunittestslice',
-#                 'description', island, project.owner)
-#             slice_change_description(slice_obj, 'new_description')
-#         except Exception, ex:
-#             print ex
-#             self.assertFalse(True)
-#         else:
-#             self.assertEqual('new_description', slice_obj.description)
-# 
-#     def test_delete_slice_api(self):
-#         print 'test_delete_slice_api'
-#         project = Project.objects.all()[0]
-#         island = project.islands.all()[0]
-#         ovs_ports = SwitchPort.objects.all()
-#         try:
-#             slice_obj = create_slice_step(project,
-#                 'cjxunittestslice', 'description',
-#                 island, project.owner,
-#                 ovs_ports, island.controller_set.all()[0])
-#             delete_slice_api(slice_obj)
-#         except Exception, ex:
-#             print ex
-#             flowvisor_del_slice(island.flowvisor_set.all()[0], 'cjxunittestslice')
-#             self.assertFalse(True)
-#         else:
-#             if not Slice.objects.filter(name='cjxunittestslice'):
-#                 self.assertTrue(True)
-#             else:
-#                 flowvisor_del_slice(island.flowvisor_set.all()[0], 'cjxunittestslice')
-#                 self.assertFalse(True)
-# 
-#     def test_start_slice_api(self):
-#         print 'test_start_slice_api'
-#         project = Project.objects.all()[0]
-#         island = project.islands.all()[0]
-#         ovs_ports = SwitchPort.objects.all()
-#         try:
-#             slice_obj = create_slice_step(project,
-#                 'cjxunittestslice', 'description',
-#                 island, project.owner,
-#                 ovs_ports, island.controller_set.all()[0])
-#             start_slice_api(slice_obj)
-#         except Exception, ex:
-#             print ex
-#             flowvisor_del_slice(island.flowvisor_set.all()[0], 'cjxunittestslice')
-#             self.assertFalse(True)
-#         else:
-#             flowvisor_del_slice(island.flowvisor_set.all()[0], 'cjxunittestslice')
-#             self.assertEqual(SLICE_STATE_STARTED, slice_obj.state)
-# 
-#     def test_stop_slice_api(self):
-#         print 'test_stop_slice_api'
-#         project = Project.objects.all()[0]
-#         island = project.islands.all()[0]
-#         ovs_ports = SwitchPort.objects.all()
-#         try:
-#             slice_obj = create_slice_step(project,
-#                 'cjxunittestslice', 'description',
-#                 island, project.owner,
-#                 ovs_ports, island.controller_set.all()[0])
-#             start_slice_api(slice_obj)
-#             stop_slice_api(slice_obj)
-#         except Exception, ex:
-#             print ex
-#             flowvisor_del_slice(island.flowvisor_set.all()[0], 'cjxunittestslice')
-#             self.assertFalse(True)
-#         else:
-#             flowvisor_del_slice(island.flowvisor_set.all()[0], 'cjxunittestslice')
-#             self.assertEqual(SLICE_STATE_STOPPED, slice_obj.state)
-# 
-#     def test_update_slice_virtual_network(self):
-#         print 'test_update_slice_virtual_network'
-#         project = Project.objects.all()[0]
-#         island = project.islands.all()[0]
-#         ovs_ports = SwitchPort.objects.all()
-#         try:
-#             slice_obj = create_slice_step(project,
-#                 'cjxunittestslice', 'description',
-#                 island, project.owner,
-#                 ovs_ports, island.controller_set.all()[0])
-#             flowspace_nw_add(slice_obj, [], '192.168.5.34/24')
-#             update_slice_virtual_network(slice_obj)
-#         except Exception, ex:
-#             print ex
-#             flowvisor_del_slice(island.flowvisor_set.all()[0], 'cjxunittestslice')
-#             self.assertFalse(True)
-#         else:
-#             flowvisor_del_slice(island.flowvisor_set.all()[0], 'cjxunittestslice')
-#             self.assertTrue(True)
+    def test_create_slice_step(self):
+        print 'test_create_slice_step'
+        project = Project.objects.all()[0]
+        island = project.islands.all()[0]
+        ovs_ports = SwitchPort.objects.all()
+        try:
+            slice_obj = create_slice_step(project,
+                'cjxunittestslice', 'description',
+                island, project.owner,
+                ovs_ports, island.controller_set.all()[0])
+        except Exception, ex:
+            print ex
+            flowvisor_del_slice(island.flowvisor_set.all()[0], 'cjxunittestslice')
+            self.assertFalse(True)
+        else:
+            flowvisor_del_slice(island.flowvisor_set.all()[0], 'cjxunittestslice')
+            self.assertTrue(slice_obj.id)
+
+    def test_create_slice_api(self):
+        print 'test_create_slice_api'
+        project = Project.objects.all()[0]
+        island = project.islands.all()[0]
+        try:
+            slice_obj = create_slice_api(project, 'cjxunittestslice',
+                'description', island, project.owner)
+        except Exception, ex:
+            print ex
+            self.assertFalse(True)
+        else:
+            self.assertTrue(slice_obj.id)
+
+    def test_slice_change_description(self):
+        print 'test_slice_change_description'
+        project = Project.objects.all()[0]
+        island = project.islands.all()[0]
+        try:
+            slice_obj = create_slice_api(project, 'cjxunittestslice',
+                'description', island, project.owner)
+            slice_change_description(slice_obj, 'new_description')
+        except Exception, ex:
+            print ex
+            self.assertFalse(True)
+        else:
+            self.assertEqual('new_description', slice_obj.description)
+
+    def test_delete_slice_api(self):
+        print 'test_delete_slice_api'
+        project = Project.objects.all()[0]
+        island = project.islands.all()[0]
+        ovs_ports = SwitchPort.objects.all()
+        try:
+            slice_obj = create_slice_step(project,
+                'cjxunittestslice', 'description',
+                island, project.owner,
+                ovs_ports, island.controller_set.all()[0])
+            delete_slice_api(slice_obj)
+        except Exception, ex:
+            print ex
+            flowvisor_del_slice(island.flowvisor_set.all()[0], 'cjxunittestslice')
+            self.assertFalse(True)
+        else:
+            if not Slice.objects.filter(name='cjxunittestslice'):
+                self.assertTrue(True)
+            else:
+                flowvisor_del_slice(island.flowvisor_set.all()[0], 'cjxunittestslice')
+                self.assertFalse(True)
+
+    def test_start_slice_api(self):
+        print 'test_start_slice_api'
+        project = Project.objects.all()[0]
+        island = project.islands.all()[0]
+        ovs_ports = SwitchPort.objects.all()
+        try:
+            slice_obj = create_slice_step(project,
+                'cjxunittestslice', 'description',
+                island, project.owner,
+                ovs_ports, island.controller_set.all()[0])
+            start_slice_api(slice_obj)
+        except Exception, ex:
+            print ex
+            flowvisor_del_slice(island.flowvisor_set.all()[0], 'cjxunittestslice')
+            self.assertFalse(True)
+        else:
+            flowvisor_del_slice(island.flowvisor_set.all()[0], 'cjxunittestslice')
+            self.assertEqual(SLICE_STATE_STARTED, slice_obj.state)
+
+    def test_stop_slice_api(self):
+        print 'test_stop_slice_api'
+        project = Project.objects.all()[0]
+        island = project.islands.all()[0]
+        ovs_ports = SwitchPort.objects.all()
+        try:
+            slice_obj = create_slice_step(project,
+                'cjxunittestslice', 'description',
+                island, project.owner,
+                ovs_ports, island.controller_set.all()[0])
+            start_slice_api(slice_obj)
+            stop_slice_api(slice_obj)
+        except Exception, ex:
+            print ex
+            flowvisor_del_slice(island.flowvisor_set.all()[0], 'cjxunittestslice')
+            self.assertFalse(True)
+        else:
+            flowvisor_del_slice(island.flowvisor_set.all()[0], 'cjxunittestslice')
+            self.assertEqual(SLICE_STATE_STOPPED, slice_obj.state)
+
+    def test_update_slice_virtual_network(self):
+        print 'test_update_slice_virtual_network'
+        project = Project.objects.all()[0]
+        island = project.islands.all()[0]
+        ovs_ports = SwitchPort.objects.all()
+        try:
+            slice_obj = create_slice_step(project,
+                'cjxunittestslice', 'description',
+                island, project.owner,
+                ovs_ports, island.controller_set.all()[0])
+            flowspace_nw_add(slice_obj, [], '192.168.5.34/24')
+            update_slice_virtual_network(slice_obj)
+        except Exception, ex:
+            print ex
+            flowvisor_del_slice(island.flowvisor_set.all()[0], 'cjxunittestslice')
+            self.assertFalse(True)
+        else:
+            flowvisor_del_slice(island.flowvisor_set.all()[0], 'cjxunittestslice')
+            self.assertTrue(True)
 
     def test_get_slice_topology(self):
         print 'test_get_slice_topology'
