63b783d5ac9aa5c37dccb8e127d5a2ae5e8254be
fay <toozoofoo@gmail.com>
2013-09-23 17:00:28 +0800

    translation

--- ccf/settings.py
+++ ccf/settings.py
@@ -178,7 +178,7 @@ LOGGING = {
         'ceni_debug': {
             'level':'DEBUG',
             'class':'logging.handlers.RotatingFileHandler',
-            'filename': 'logs/ceni_debug.log',
+            'filename': PROJECT_ROOT + '/logs/ceni_debug.log',
             'maxBytes': 1024*1024*5,
             'backupCount': 5,
             'formatter':'standard',
@@ -186,7 +186,7 @@ LOGGING = {
         'ceni_error': {
             'level':'ERROR',
             'class':'logging.handlers.RotatingFileHandler',
-            'filename': 'logs/ceni_error.log',
+            'filename': PROJECT_ROOT + '/logs/ceni_error.log',
             'maxBytes': 1024*1024*5,
             'backupCount': 5,
             'formatter':'standard',
