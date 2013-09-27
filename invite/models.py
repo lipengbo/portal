b9e00c992b5df41d37b0e9d8de309fe29d11ff9a
tengzhifei <tengzhifei@fnic.cn>
2013-09-27 14:20:43 +0800

    project apply

--- invite/models.py
+++ invite/models.py
@@ -20,9 +20,9 @@ class InvitationManager(models.Manager):
                 message=message, target=target)
         invitation.save()
 
-class Invitation(models.Model):
-    from_user = models.ForeignKey(User, related_name="from_invitations")
-    to_user = models.ForeignKey(User, related_name="to_invitations", verbose_name=_("Invitee"))
+class Connection(models.Model):
+    from_user = models.ForeignKey(User, related_name="%(app_label)s_%(class)s_from_invitations")
+    to_user = models.ForeignKey(User, related_name="%(app_label)s_%(class)s_to_invitations", verbose_name=_("Invitee"))
 
     message = models.TextField(verbose_name=_("Message"))
     accepted = models.BooleanField(default=False)
@@ -32,16 +32,10 @@ class Invitation(models.Model):
     target_id = models.PositiveIntegerField()
     target = generic.GenericForeignKey('target_type', 'target_id')
 
-    objects = InvitationManager()
-
     def accept(self):
         self.target.accept(self.to_user)
         self.accepted = True
         self.save()
-
-    def accept_link(self):
-        link = "http://%(domain)s%(relative_link)s" % ({"domain": Site.objects.get_current(), "relative_link": reverse("invite_accept", args=(self.key, ))})
-        return link
 
     def get_target_name(self):
         display_name_func = getattr(self.target, 'get_display_name')
@@ -51,11 +45,35 @@ class Invitation(models.Model):
             name = self.target.__unicode__()
         return name
 
+    class Meta:
+
+        abstract = True
+
+class Invitation(Connection):
+
+    objects = InvitationManager()
+
+    def accept_link(self):
+        link = "http://%(domain)s%(relative_link)s" % ({"domain": Site.objects.get_current(), "relative_link": reverse("invite_accept", args=("invite", self.key, ))})
+        return link
+
+    def send(self):
+        body = _("You're invited by %(inviter)s to join a project of %(project)s.\nHere is a message from %(inviter)s:\n%(message)s\nYou can click the link below to accept the invitation:\n%(accept_link)s") % ({"inviter": self.from_user, "project": self.get_target_name(), "message": self.message, "accept_link": self.accept_link()})
+        send_mail(_("You have an invitation"), body, self.from_user.email, [self.to_user.email])
+
+class Application(Connection):
+
+    def accept_link(self):
+        link = "http://%(domain)s%(relative_link)s" % ({"domain": Site.objects.get_current(), "relative_link": reverse("invite_accept", args=("apply", self.key, ))})
+        return link
+
+
     def send(self):
         body = _("You're invited by %(inviter)s to join a project of %(project)s.\nHere is a message from %(inviter)s:\n%(message)s\nYou can click the link below to accept the invitation:\n%(accept_link)s") % ({"inviter": self.from_user, "project": self.get_target_name(), "message": self.message, "accept_link": self.accept_link()})
         send_mail(_("You have an invitation"), body, self.from_user.email, [self.to_user.email])
 
 @receiver(pre_save, sender=Invitation)
+@receiver(pre_save, sender=Application)
 def generate_key(sender, instance, **kwargs):
     if not instance.id:
         salt = "asj3r3289s9823"
@@ -64,6 +82,7 @@ def generate_key(sender, instance, **kwargs):
             instance.target_id, salt)).hexdigest()
 
 @receiver(post_save, sender=Invitation)
+@receiver(post_save, sender=Application)
 def send_invite(sender, instance, created, **kwargs):
     if created:
         instance.send()
--- invite/views.py
+++ invite/views.py
@@ -12,7 +12,7 @@ from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes import generic
 from django.contrib import messages
 
-from invite.models import Invitation
+from invite.models import Invitation, Application
 from invite.forms import InvitationForm
 
 @login_required
@@ -38,8 +38,12 @@ def invite(request, target_type_id, target_id):
     context['form'] = form
     return render(request, "invite/invite.html", context)
 
-def accept(request, key):
-    invitation = get_object_or_404(Invitation, key=key)
+def accept(request, kind="invite", key=""):
+    if kind == 'apply':
+        ModelClass = Application
+    else:
+        ModelClass = Invitation
+    invitation = get_object_or_404(ModelClass, key=key)
     invitation.accept()
 
     user = request.user
