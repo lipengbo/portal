694d31925c111875f064ed8081a1ddb44d457a5c
cjx <cjx@cjx.cjx>
2013-10-18 15:03:47 +0800

    slice topology

---
+++ ccf/local_setting.py
@@ -0,0 +1,10 @@
+iujkjkDATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'ceni',  
+        'USER': 'root',  
+        'PASSWORD': 'cdn%nf',  
+        'HOST': '192.168.5.120',  
+        'PORT': '3306',  
+    }
+}
