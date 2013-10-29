f9557910b0891d2301c8039397cfea9850125000
chengwen <chengwen@chengwen-pc.(none)>
2013-10-29 10:13:32 +0800

    修改网卡监控功能

--- slice/views.py
+++ slice/views.py
@@ -321,9 +321,9 @@ def update_vm_performace_data(request):
     else:
         for (key, value), bps_data in zip(vm_perf_data["net"].items(), pre_net_data):
             net_data[key] = [value[0], value[1],
-                           #value[0] - int(bps_data.split(':')[0]),
-                           #value[1] - int(bps_data.split(':')[1])]
-                            200, 300]
+                           value[0] - int(bps_data.split(':')[0]),
+                           value[1] - int(bps_data.split(':')[1])]
+                            #200, 300]
 
 
     disk_data = {"free" : int(vm_perf_data["disk"]["free"]/8/1024/1024), "used" : int(vm_perf_data["disk"]["used"]/8/1024/1024)}
