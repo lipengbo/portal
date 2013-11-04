62acbcac36f70372487a7fcb295ded67c6f6eacd
chengwen <chengwen@chengwen-pc.(none)>
2013-11-04 09:30:45 +0800

    modify monitor feature

--- monitor/views.py
+++ monitor/views.py
@@ -46,16 +46,29 @@ def update_port_performace_data(request):
 
     switch = get_object_or_404(Switch, id = switch_id)
     switch_stat = get_switch_stat(switch.ip)
-    recv_data = 0
-    send_data = 0
+    #recv_data = 0
+    #send_data = 0
     for br in switch_stat:
         if br['name'] == br_name:
             for port in br['ports']:
                 if port['name'] == port_name:
+                    print "\n",port_name
+                    print "----------------------------"
+                    print "pre_recv_data = ", pre_recv_data
+                    print "pre_send_data = ", pre_send_data
                     recv_data = int(port['stats']['recv']['byte'])
                     send_data = int(port['stats']['send']['byte'])
+                    print "recv_data =", recv_data
+                    print "send_data =", send_data
+    recv_bps = recv_data - int(pre_recv_data)
+    send_bps = send_data - int(pre_send_data)
+    #在不同网卡间切换的时候，得到的数据差值有可能为负，故做此处理
+    if recv_bps < 0 :
+        recv_bps = 0
+    if send_bps < 0 :
+        send_bps = 0
     performace_port_data = {'port_recv_data' : recv_data, 'port_send_data' : send_data,
-                            'recv_bps' : recv_data - int(pre_recv_data), 'send_bps' : send_data - int(pre_send_data)}
+                            'recv_bps' : recv_bps, 'send_bps' : send_bps}
     return HttpResponse(json.dumps(performace_port_data))
 
 
@@ -69,34 +82,30 @@ def update_vm_performace_data(request):
     agent_ip = vm.server.ip
     agent = AgentClient(ip = agent_ip)
     vm_perf_data = json.loads(agent.get_domain_status(vm.uuid))
-
-
-
-
-
-
-
-
     # vm_perf_data = {"mem": {"total": 262144, "percent": 100, "free": 0, "used": 262144},
-    # "net": {"4f6f91d4": [5522, 984, 7080755, 12, 0, 0, 0, 0],
-    #        "4f6f91d5": [123, 84, 0755, 12, 0, 0, 0, 0]},
-    #"disk": {"total": 858993459200.0, "percent": 0.067138671875, "free": 8416742400.0, "used": 576716800.0},
-    #"cpu": 0.0}
+    #               "net": {"4f6f91d4": [5522, 984, 7080755, 12, 0, 0, 0, 0],
+    #               "4f6f91d5": [123, 84, 0755, 12, 0, 0, 0, 0]},
+    #               "disk": {"total": 858993459200.0, "percent": 0.067138671875, "free": 8416742400.0, "used": 576716800.0},
+    #               "cpu": 0.0}
     net_data = {}
     if pre_net_data[0] == '':
         for (key, value) in vm_perf_data["net"].items():
             net_data[key] = [value[0], value[1], 0, 0]
     else:
         for (key, value), bps_data in zip(vm_perf_data["net"].items(), pre_net_data):
-            net_data[key] = [value[0], value[1],
-                           value[0] - int(bps_data.split(':')[0]),
-                           value[1] - int(bps_data.split(':')[1])]
-                            #200, 300]
+            bps_recv = value[0] - int(bps_data.split(':')[0])
+            bps_send = value[1] - int(bps_data.split(':')[1])
+
+            if bps_recv < 0:
+                bps_recv = 0
+            if bps_send < 0:
+                bps_send = 0
+            net_data[key] = [value[0], value[1], bps_recv, bps_send]
 
 
     domain_disk_data = {"free" : int(vm_perf_data["disk"]["free"]),
                         "used" : int(vm_perf_data["disk"]["used"])}
-    #print net_data
+
     return HttpResponse(json.dumps({'cpu_use' : vm_perf_data["cpu"],
                                     'mem_use' : vm_perf_data["mem"]["percent"],
                                     'net' : net_data,
@@ -116,9 +125,17 @@ def update_host_performace_data(request):
             net_data[key] = [value[0], value[1], 0, 0]
     else:
         for (key, value), bps_data in zip(host_perf_data["net"].items(), pre_net_data):
-            net_data[key] = [value[0], value[1],
-                           value[0] - int(bps_data.split(':')[0]),
-                           value[1] - int(bps_data.split(':')[1])]
+            bps_recv = value[0] - int(bps_data.split(':')[0])
+            bps_send = value[1] - int(bps_data.split(':')[1])
+
+            if bps_recv < 0:
+                bps_recv = 0
+            if bps_send < 0:
+                bps_send = 0
+            net_data[key] = [value[0], value[1], bps_recv, bps_send]
+            #net_data[key] = [value[0], value[1],
+             #              value[0] - int(bps_data.split(':')[0]),
+              #             value[1] - int(bps_data.split(':')[1])]
 
     for (key, value) in host_perf_data["disk"].items():
         host_disk_data = {"free" : int(value[2]), "used" : int(value[1])}
