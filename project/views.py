b8d84b2577784d09f1896b7daf4cb8492c5a324d
tengzhifei <tengzhifei@fnic.cn>
2013-11-27 16:15:07 +0800

    invite list

--- project/views.py
+++ project/views.py
@@ -115,9 +115,9 @@ def invite(request, id):
             messages.add_message(request, messages.ERROR,
                     _("Invitation message is required."))
 
-    invited_user_ids = list(Invitation.objects.filter(target_id=project.id,
-            target_type=target_type).values_list("to_user__id", flat=True))
-    invited_user_ids.extend(project.member_ids())
+    invited_user_ids = []#list(Invitation.objects.filter(target_id=project.id,
+            #target_type=target_type).values_list("to_user__id", flat=True))
+    #invited_user_ids.extend(project.member_ids())
     invited_user_ids.extend(User.objects.filter(is_superuser=True).values_list("id", flat=True))
     users = User.objects.exclude(id__in=set(invited_user_ids))
     if 'query' in request.GET:
