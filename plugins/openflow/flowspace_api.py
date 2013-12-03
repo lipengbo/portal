6e4460f62f1cf29de4f7c30b37ed5926ddccc6b3
cjx <cjx@cjx.cjx>
2013-12-03 13:46:15 +0800

    slice`

--- plugins/openflow/flowspace_api.py
+++ plugins/openflow/flowspace_api.py
@@ -2,7 +2,7 @@
 from models import *
 from django.db import transaction
 from slice.slice_exception import DbError
-from plugins.vt.api.py import get_slice_gw_mac, get_phydata_gw_mac
+from plugins.vt.api import get_slice_gw_mac, get_phydata_gw_mac
 
 
 import logging
