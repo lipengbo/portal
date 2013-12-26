de99ee4d8e567abb7233d75671938e7f968268f7
tengzhifei <tengzhifei@fnic.cn>
2013-12-26 14:55:21 +0800

    add view slice permission

--- project/models.py
+++ project/models.py
@@ -145,9 +145,6 @@ class Project(models.Model):
     class Meta:
         verbose_name = _("Project")
         permissions = (
-                ('create_slice', _("Can add Slice")),
-                ('delete_slice', _("Can delete Slice")),
-                ('edit_slice', _("Can edit Slice")),
                 #('manage_project_member', _('Manage Project Member')),
                 #('invite_project_member', _('Invite Project Member')),
                 #('dismiss_project_member', _('Dismiss Project Member')),
--- slice/models.py
+++ slice/models.py
@@ -252,6 +252,7 @@ class Slice(models.Model):
 
     class Meta:
         permissions = (
+            ('view_slice', _('View Slice')),
         )
 
 class SliceDeleted(models.Model):
