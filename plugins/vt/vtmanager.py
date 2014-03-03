82d029c957ff1c4c016fd3e5621e09ece1fdf18d
lipengbo <lipengbo10054444@gmail.com>
2014-03-03 16:04:53 +0800

    修改schedul算法

--- plugins/vt/vtmanager.py
+++ plugins/vt/vtmanager.py
@@ -18,23 +18,26 @@ class Filter(object):
         self.vcpu = int(vcpu)
 
     def check_resource_by_monitor(self, hostid, hostip):
-        client = AgentClient(hostip)
-        host_status = json.loads(client.get_host_status())
-        if not config.function_test:
-            if config.unique_hosts_per_alloc <= client.get_instances_count():
-                return False
-            if self.vcpu > int(client.get_host_info()['vcpus']):
-                return False
-            if config.max_cpu < float(host_status['cpu']):
-                return False
-            mem_free = float(host_status['mem'][4])
-            mem_total = float(host_status['mem'][0])
-            if config.max_mem < (mem_total - mem_free - (self.mem << 20)) * 100 / mem_total:
-                return False
-            if config.max_disk != 0:
-                disk_free = int(host_status['disk'].items()[0][1][2])
-                if config.max_disk > (disk_free >> 30) - self.disk:
+        try:
+            client = AgentClient(hostip)
+            host_status = json.loads(client.get_host_status())
+            if not config.function_test:
+                if config.unique_hosts_per_alloc <= client.get_instances_count():
                     return False
+                if self.vcpu > int(client.get_host_info()['vcpus']):
+                    return False
+                if config.max_cpu < float(host_status['cpu']):
+                    return False
+                mem_free = float(host_status['mem'][1])
+                mem_total = float(host_status['mem'][0])
+                if config.max_mem < (mem_total - mem_free + (self.mem << 20)) * 100 / mem_total:
+                    return False
+                if config.max_disk != 0:
+                    disk_free = int(host_status['disk'].items()[0][1][2])
+                    if config.max_disk > (disk_free >> 30) - self.disk:
+                        return False
+        except:
+            return False
         return hostid
 
     def filter(self, host_list):
@@ -42,4 +45,6 @@ class Filter(object):
             host = Server.objects.get(id=hostid)
             if host.state and self.check_resource_by_monitor(hostid, hostip):
                 return hostid
+            else:
+                continue
         return False
