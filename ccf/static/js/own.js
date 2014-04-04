c53cb9a488c70c65351deeb5210112ace8124a6f
cjx <cjx@cjx.cjx>
2014-04-04 09:34:36 +0800

    slice

--- ccf/static/js/own.js
+++ ccf/static/js/own.js
@@ -94,7 +94,6 @@ $(document).ready(function() {
     });
     //slice步骤切换
     $(".next_btn").click(function(){
-        alert("r");
         $('.no-virtual-switch').hide();
         if ($(this).hasClass('btn-step2')) {
             var has_virtual_switch = false;
@@ -104,7 +103,6 @@ $(document).ready(function() {
                 }
             }
             if (!has_virtual_switch) {
-                alert("p");
                 $('.no-virtual-switch').show();
                 return false;
             }
@@ -120,7 +118,6 @@ $(document).ready(function() {
            }
        }
        if(thisIndex == 1){
-           alert(0);
        	   ret = page_function1();
        	   if (!ret){
            		return;
@@ -282,9 +279,7 @@ function page_function0(){
 function page_function1(){
 	
 	//ret1 = check_switch_port();
-	alert(1);
 	var ret2 = check_nw_num();
-	alert(2);
 	//alert(ret2);
 	if (ret2){
 		
@@ -313,14 +308,12 @@ function page_function2(){
 	ret1 = check_slice_controller('controller_type') && check_gw_select();
 
 	if(!document.getElementById('dhcp_selected').checked){
-	    alert(12);
 		var objs = document.getElementsByName("dhcp");
 		//$('[name="enable_dhcp"]').Check('uncheck');
 		for(var i=0; i<objs.length; i++){
 			objs[i].style.display = "none";
 		}
 	}else{
-	    alert(13);
 		var objs = document.getElementsByName("dhcp");
 		//$('[name="enable_dhcp"]').Check('check');		
 		for(var i=0; i<objs.length; i++){
