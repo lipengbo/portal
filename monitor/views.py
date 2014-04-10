1d8a85ce5807b9f510d318d75400a916318fcc69
chengwen <chengwen@fnic.cn>
2014-04-10 13:39:02 +0800

    fix bugs

--- monitor/views.py
+++ monitor/views.py
@@ -163,9 +163,6 @@ def update_host_performace_data(request):
         server = get_object_or_404(Server, id = host_id)
         agent = AgentClient(ip = server.ip)
         host_perf_data = json.loads(agent.get_host_status())
-        print "----------------------"
-        print host_perf_data
-        print "----------------------"
         net_data = {}
         if pre_net_data[0] == '':
             for (key, value) in host_perf_data["net"].items():
