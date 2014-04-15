3292a966a8e7b8ec027ac7b9fbe95694eef53303
lipengbo <lipengbo10054444@gmail.com>
2014-04-15 17:37:19 +0800

    添加aes加密算法

--- etc/config.py
+++ etc/config.py
@@ -23,7 +23,7 @@ def generate_glance_url():
 #[plugin-advance]
 #高级配置项，用于配置文件锁的位置
 lock_path = '/var/run/'
-default_flavor_id = 2
+default_flavor_id = 1
 rpc_connection_timeout = 150
 domain_count_infinity = 10000
 #配置slice的网关与物理网关的通信方式，True表示通过slice控制器控制（下相应flowspace），False表示不通过控制器控制。
@@ -33,11 +33,11 @@ slice_expiration_days = 5
 #单元测试的时候使用，当系统发布的时候该值必须为False
 function_test = False
 #配置系统是否使用flowvisor，当系统发布的时候该值必须为False
-flowvisor_disable = False
+flowvisor_disable = True
 #配置系统虚网划分使用flowvisor还是cnvp
 flowvisor_or_cnvp = "cnvp"
 #本期可以不用部署vt_manager；当use_vt_manager_to_schedul = False不需部署vt_manager；当use_vt_manager_to_schedul = True需要部署vt_manager；
-use_vt_manager_to_schedul = False
+use_vt_manager_to_schedul = True
 #[scheduler]只有在use_vt_manager_to_schedul = False时才生效
 #单台机器最多允许创建的虚拟机的数量
 unique_hosts_per_alloc = 100
@@ -49,3 +49,5 @@ max_disk = 0
 #[vt_manager]只有在use_vt_manager_to_schedul = True时才生效
 vt_manager_ip = '127.0.0.1'
 vt_manager_port = 8891
+#aes字符长度必须为16位
+aes_key = 'fnic123456789012'
