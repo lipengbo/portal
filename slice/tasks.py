c54aa811bd84ec0d09191d1a1200c9be0f3241bf
cjx <cjx@cjx.cjx>
2014-04-28 14:07:13 +0800

    slice

--- slice/tasks.py
+++ slice/tasks.py
@@ -13,11 +13,13 @@ def add(x, y):
 
 @task()
 def start_slice_sync(slice_id, controller_flag, gw_flag):
+    print "start_slice_sync"
     flag = False
     ct_op = True
     gw_op = True
     try:
         slice_obj = Slice.objects.get(id=slice_id)
+        print "start controller"
         if controller_flag:
             controller = slice_obj.get_controller()
             if controller.host:
@@ -28,6 +30,7 @@ def start_slice_sync(slice_id, controller_flag, gw_flag):
                     controller.host.state = DOMAIN_STATE_DIC['shutoff']
                     ct_op = False
                 controller.host.save()
+        print "start gw"
         if gw_flag:
             gw = slice_obj.get_gw()
             if gw and gw.enable_dhcp:
@@ -38,12 +41,16 @@ def start_slice_sync(slice_id, controller_flag, gw_flag):
                     gw.state = DOMAIN_STATE_DIC['shutoff']
                     gw_op = False
                 gw.save()
+        print "stop slice, update flowspace, start slice"
         if ct_op and gw_op:
             flowvisor = slice_obj.get_flowvisor()
             if flowvisor.type == 1:
+                print 1
                 flowvisor_update_slice_status(flowvisor,
                                               slice_obj.id, False)
+                print 2
                 update_slice_virtual_network_cnvp(slice_obj)
+                print 3
                 flowvisor_update_slice_status(flowvisor,
                                               slice_obj.id, True)
                 flag = True
@@ -53,6 +60,7 @@ def start_slice_sync(slice_id, controller_flag, gw_flag):
                 flag = True
                 update_slice_virtual_network_flowvisor(slice_obj)
             slice_obj.start()
+        print "start success"
     except Slice.DoesNotExist:
         pass
     except Exception, ex:
@@ -68,6 +76,7 @@ def start_slice_sync(slice_id, controller_flag, gw_flag):
 
 @task()
 def stop_slice_sync(slice_id):
+    print "stop_slice_sync"
     try:
         slice_obj = Slice.objects.get(id=slice_id)
         if slice_obj.state == 3:
