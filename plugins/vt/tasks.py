90d1b356536def9bddc0e1a681fc2cfb2ed932b6
chengwen <chengwen@fnic.cn>
2014-04-28 16:26:11 +0800

    cnvp http

--- plugins/vt/tasks.py
+++ plugins/vt/tasks.py
@@ -3,21 +3,26 @@ from models import DOMAIN_STATE_DIC
 
 @task()
 def do_vm_action(vm, action):
+    print "cerlry .............", action
     try:
-        print "cerlry .............", action
         action_result = vm.do_action(action)
-        if action_result:
-            if action == "destroy":
-                vm.state = DOMAIN_STATE_DIC['shutoff']
-            else:
-                vm.state = DOMAIN_STATE_DIC['running']
-        else:
-            if action == "destroy":
-                vm.state = DOMAIN_STATE_DIC['running']
-            else:
-                vm.state = DOMAIN_STATE_DIC['shutoff']
-        vm.save()
-        print "---------------vm state : ", vm.state
-        #return result
+        reset_state(vm, action, action_result)
     except:
-        pass
+        reset_state(vm, action, False)
+    finally:
+        print "---------------vm state : ", vm.state
+
+
+def reset_state(vm, action, result):
+    if result:
+        if action == "destroy":
+            vm.state = DOMAIN_STATE_DIC['shutoff']
+        else:
+            vm.state = DOMAIN_STATE_DIC['running']
+    else:
+        if action == "destroy":
+            vm.state = DOMAIN_STATE_DIC['running']
+        else:
+            vm.state = DOMAIN_STATE_DIC['shutoff']
+    vm.save()
+
