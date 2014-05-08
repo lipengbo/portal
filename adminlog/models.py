bef7e6be4926d22b101daacacac8b4ca34b19b32
tengzhifei <tengzhifei@fnic.cn>
2014-05-08 17:03:34 +0800

    admin log

---
+++ adminlog/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.db.models import F
+from django.contrib.admin.models import LogEntry, ADDITION
+from django.contrib.contenttypes.models import ContentType
+
+SUCCESS = 200
+FAIL = 201
+
+# Create your models here.
+def log(user, target, message, result_code=SUCCESS):
+    LogEntry.objects.log_action(user_id=user.id,
+            content_type_id=ContentType.objects.get_for_model(target).pk,
+            object_id=target.pk,
+            object_repr=target.__unicode__(),
+            action_flag=result_code,
+            change_message=message)
