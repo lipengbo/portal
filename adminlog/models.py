da52b0a0d8db72898e850c28b30fa6dbe122f1af
tengzhifei <tengzhifei@fnic.cn>
2014-05-14 10:45:34 +0800

    allow null target

--- adminlog/models.py
+++ adminlog/models.py
@@ -10,9 +10,12 @@ FAIL = 201
 
 # Create your models here.
 def log(user, target, message, result_code=SUCCESS):
+    content_type_id = target and ContentType.objects.get_for_model(target).pk or None
+    object_id = target and target.pk or None
+    object_repr = target and target.__unicode__() or ''
     LogEntry.objects.log_action(user_id=user.id,
-            content_type_id=ContentType.objects.get_for_model(target).pk,
-            object_id=target.pk,
-            object_repr=target.__unicode__(),
+            content_type_id=content_type_id,
+            object_id=object_id,
+            object_repr=object_repr,
             action_flag=result_code,
             change_message=message)
