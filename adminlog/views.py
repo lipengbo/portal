fad349d516f685cf0ce5778878c33480a6ce29bb
tengzhifei <tengzhifei@fnic.cn>
2014-05-15 09:43:14 +0800

    log list page

--- adminlog/views.py
+++ adminlog/views.py
@@ -10,13 +10,18 @@ from django.template import RequestContext
 from django.utils.translation import ugettext, ugettext as _
 
 from django.contrib.admin.models import LogEntry
-
+from adminlog.models import SUCCESS, FAIL
 @login_required
 def index(request):
     context = {}
     user = request.user
-    logs = LogEntry.objects.all()
+    logs = LogEntry.objects.filter(action_flag__in=(SUCCESS, FAIL))
     if not user.is_superuser:
         logs = logs.filter(user_id=user.id)
+    if 'query' in request.GET:
+        query = request.GET.get('query')
+        if query:
+            logs = logs.filter(change_message__icontains=query)
+            context['query'] = query
     context['logs'] = logs
     return render(request, 'adminlog/index.html', context)
