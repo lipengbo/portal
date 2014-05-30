201424e23c2f1f23388699bd89e6a8347cb4f216
chengwen <chengwen@fnic.cn>
2014-05-30 09:01:38 +0800

    monitor switch

--- monitor/views.py
+++ monitor/views.py
@@ -211,16 +211,17 @@ def sflow_list_ports(request, switch_id):
     ports = {}
     switch = get_object_or_404(Switch, id=switch_id)
     print "**********switch_id:", switch.ip
-    #SFlow_Proxy.list_ports(switch.ip)
-    ports = {"1": ("up", 1000), "2": ("up", 2000), "3":("down", 0)}
+    ports = SFlow_Proxy.list_ports(switch.ip)
+    print "**********ports:", ports
+    #ports = {"1": ("up", 1000), "2": ("up", 2000), "3":("down", 0)}
     return HttpResponse(json.dumps(ports))
 
 def sflow_get_bps(request, switch_id, port):
     try:
         print "*****************port is ", port
         switch = get_object_or_404(Switch, id=switch_id)
-        #in_bps, out_bps = SFlow_Proxy.get_switch_port_bps(switch.ip, int(port))
-        return HttpResponse(json.dumps({'result':0, 'in_bps': int(random.random()*1000), \
-                                    'out_bps': int(random.random()*1000)}))
+        in_bps, out_bps = SFlow_Proxy.get_switch_port_bps(switch.ip, int(port))
+        return HttpResponse(json.dumps({'result':0, 'in_bps': in_bps, \
+                                    'out_bps': out_bps}))
     except:
         return HttpResponse(json.dumps({'result': 1}))
