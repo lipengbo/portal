bca4933ce73552dfd7a184527ce22793c34f1b57
cjx <cjx@cjx.cjx>
2014-07-11 13:52:36 +0800

    slice

---
+++ project/migrations/0009_auto__add_field_projectquota_max_member__add_field_projectquota_max_sl.py
@@ -0,0 +1,147 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'ProjectQuota.max_member'
+        db.add_column('project_projectquota', 'max_member',
+                      self.gf('django.db.models.fields.IntegerField')(default=100, null=True),
+                      keep_default=False)
+
+        # Adding field 'ProjectQuota.max_slice'
+        db.add_column('project_projectquota', 'max_slice',
+                      self.gf('django.db.models.fields.IntegerField')(default=10, null=True),
+                      keep_default=False)
+
+        # Adding field 'ProjectQuota.max_vm'
+        db.add_column('project_projectquota', 'max_vm',
+                      self.gf('django.db.models.fields.IntegerField')(default=256, null=True),
+                      keep_default=False)
+
+        # Adding field 'ProjectQuota.max_band'
+        db.add_column('project_projectquota', 'max_band',
+                      self.gf('django.db.models.fields.IntegerField')(default=1000, null=True),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'ProjectQuota.max_member'
+        db.delete_column('project_projectquota', 'max_member')
+
+        # Deleting field 'ProjectQuota.max_slice'
+        db.delete_column('project_projectquota', 'max_slice')
+
+        # Deleting field 'ProjectQuota.max_vm'
+        db.delete_column('project_projectquota', 'max_vm')
+
+        # Deleting field 'ProjectQuota.max_band'
+        db.delete_column('project_projectquota', 'max_band')
+
+
+    models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'project.category': {
+            'Meta': {'object_name': 'Category'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.CharField', [], {'default': "'img/cat_other.png'", 'max_length': '32'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '64'})
+        },
+        'project.city': {
+            'Meta': {'object_name': 'City'},
+            'description': ('django.db.models.fields.TextField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '128'})
+        },
+        'project.island': {
+            'Meta': {'object_name': 'Island'},
+            'city': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['project.City']"}),
+            'description': ('django.db.models.fields.TextField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '128'}),
+            'novnc_ip': ('django.db.models.fields.IPAddressField', [], {'max_length': '15', 'null': 'True'}),
+            'sflow_ip': ('django.db.models.fields.IPAddressField', [], {'default': "'0.0.0.0'", 'max_length': '15', 'null': 'True'}),
+            'sflow_port': ('django.db.models.fields.IntegerField', [], {'null': 'True'}),
+            'vpn_ip': ('django.db.models.fields.IPAddressField', [], {'default': "'0.0.0.0'", 'max_length': '15', 'null': 'True'})
+        },
+        'project.membership': {
+            'Meta': {'unique_together': "(('project', 'user'),)", 'object_name': 'Membership'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_owner': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'project': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['project.Project']"}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"})
+        },
+        'project.priority': {
+            'Meta': {'object_name': 'Priority'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'priority': ('django.db.models.fields.IntegerField', [], {'null': 'True'}),
+            'user': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['auth.User']", 'unique': 'True'})
+        },
+        'project.project': {
+            'Meta': {'unique_together': "(('owner', 'name'),)", 'object_name': 'Project'},
+            'category': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['project.Category']"}),
+            'created_time': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '1024'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_deleted': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'islands': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['project.Island']", 'symmetrical': 'False'}),
+            'memberships': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'project_belongs'", 'symmetrical': 'False', 'through': "orm['project.Membership']", 'to': "orm['auth.User']"}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'owner': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"})
+        },
+        'project.projectquota': {
+            'Meta': {'object_name': 'ProjectQuota'},
+            'band': ('django.db.models.fields.IntegerField', [], {'default': '0', 'null': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'max_band': ('django.db.models.fields.IntegerField', [], {'default': '1000', 'null': 'True'}),
+            'max_member': ('django.db.models.fields.IntegerField', [], {'default': '100', 'null': 'True'}),
+            'max_slice': ('django.db.models.fields.IntegerField', [], {'default': '10', 'null': 'True'}),
+            'max_vm': ('django.db.models.fields.IntegerField', [], {'default': '256', 'null': 'True'}),
+            'member': ('django.db.models.fields.IntegerField', [], {'default': '0', 'null': 'True'}),
+            'project': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['project.Project']", 'unique': 'True'}),
+            'slice': ('django.db.models.fields.IntegerField', [], {'default': '0', 'null': 'True'}),
+            'vm': ('django.db.models.fields.IntegerField', [], {'default': '0', 'null': 'True'})
+        }
+    }
+
+    complete_apps = ['project']
